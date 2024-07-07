c915d24619cc67322a28fe6fe8b3aa3077870576
Harsh Trivedi <harshtrivedi@Harshs-MacBook-Air.local>
2024-07-07 08:05:26 -0700

    07072024_1

--- DevOps_scripts/update_server.py
+++ DevOps_scripts/update_server.py
@@ -1,28 +1,25 @@
-def update_server_config(file_path,key,value):
-  #Read theexisting configuration of sever.conf file
-	with open(file_path,'r') as file:
-	 	lines=file.readlines()
+def update_server_config(file_path, key, value):
+    # Read the existing configuration of the server.conf file
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
- #update theconfiguration value for a specific key
-	with open(file_path,'w') as file:
-	 	for line in lines:
-			#check if the line starts with specified key
-			if key in line:
-				#update the file with new value
-				file.write(key + "=" + value + "\n")
-			else:
-				#keep the same value
-				file.write(line)
+    # Update the configuration value for a specific key
+    with open(file_path, 'w') as file:
+        for line in lines:
+            # Check if the line starts with the specified key
+            if line.startswith(key + "="):
+                # Update the line with the new value
+                file.write(key + "=" + value + "\n")
+            else:
+                # Keep the same value
+                file.write(line)
 
+# Path to server configuration file
+server_config_file = 'server.conf'
 
-#path to server configuration file 
-server_config_file='server.conf'
+# Key and new value for which the server.conf file needs to be updated
+key_to_update = 'MAX_CONNECTIONS'
+new_value = '1000'  # New maximum connections allowed
 
-#key and new value for which server.conf files need to be updated
-key_to_update='MAX_CONNECTIONS'
-new_value='1000'   #new maximum connections allowed
-
-
-#update the server.conf file
-update_server_config(server_config_file,key_to_update,new_value)
-
+# Update the server.conf file
+update_server_config(server_config_file, key_to_update, new_value)
